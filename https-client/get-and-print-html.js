fcaa74e02f423d480453704b80e249503ec3aaa1
Renato Amaral <srkimbo@gmail.com>
2018-02-19 17:47:34 +0000

    Step 2 - Printing with Buffered Strings - Completed

--- https-client/get-and-print-html.js
+++ https-client/get-and-print-html.js
@@ -1,3 +1,5 @@
+var https = require('https');
+
 function getAndPrintHTML () {
 
   var requestOptions = {
@@ -5,6 +7,22 @@ function getAndPrintHTML () {
     path: '/http-examples/step2.html'
   };
 
-  /* Add your code here */
+  https.get(requestOptions, function(response){
 
-}
+    var tmpData = "";
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      tmpData += data;
+    });
+
+    response.on('end', function() {
+      console.log('All data received after buffering .....' + tmpData);
+    });
+
+  });
+};
+
+getAndPrintHTML();
+
