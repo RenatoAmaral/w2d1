d46e4bd1b45439026c46bcf794f993a0edffebbb
Renato Amaral <srkimbo@gmail.com>
2018-02-19 17:22:40 +0000

    Step 1 - Printing in Chunks completed

---
+++ https-client/get-and-print-html-chunk.js
@@ -0,0 +1,28 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var https = require('https');
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response){
+
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length + "\n" + data + "\n");
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+
+};
+
+getAndPrintHTMLChunks();
